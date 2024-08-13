2145de82033dcd58282283457cdd1c91fe405b8c
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-08-13 17:16:40 +0200

    Add files via upload

--- src/radarcore.cpp
+++ src/radarcore.cpp
@@ -1,6 +1,9 @@
 #include <cstdint>
+#include <cstdio>
+#include <cstdlib>
 #include <raylib.h>
 #include "radarcore.hpp"
+#include <time.h>
 
 Radar::Radar(uint16_t ResX, uint16_t ResY) {
     InitWindow(ResX,ResY,"Radar");
@@ -10,14 +13,28 @@ Radar::Radar(uint16_t ResX, uint16_t ResY) {
         0,
         1
     };
+
+    Radar::PlaneSpawnTime = 0;
+    Radar::ResX = ResX;
+    Radar::ResY = ResY;
+
+    srand(time(0));
+    SetTargetFPS(60);
 }
 
 Radar::~Radar() {
     CloseWindow();
+    for(uint16_t i = 0; i < Radar::Planes.size(); ++i) {
+        DestroyPlane(i);
+    }
 }
 
 void Radar::Run() {
     while (!WindowShouldClose()) {
+        DeltaTime = GetFrameTime();
+        SpawnPlane();
+        UpdatePlanes();
+        UpdateEvent();
         UpdateRender();
     }
 }
@@ -25,7 +42,65 @@ void Radar::Run() {
 void Radar::UpdateRender() {
     BeginDrawing();
     ClearBackground(GRAY);
-    DrawRectangle(0,0,10,10,WHITE);
+    BeginMode2D(Radar::Camera);
+        
+    for(uint16_t i = 0; i < Radar::Planes.size(); ++i) {
+        DrawCircle(
+            Planes.at(i).Position.x,
+            Planes.at(i).Position.y,
+            10,
+            Planes.at(i).color
+        );
+    }
+
+    EndMode2D();
     EndDrawing();
 }
- 
+
+void Radar::SpawnPlane() {
+   if(Radar::PlaneSpawnTime <= 0 || Planes.size() < 2) {
+        char *name = new char[16];
+        sprintf(name,"PL%u",GetRandomValue(0,1000));
+        Plane pl = {
+            (Vector2){static_cast<float>(GetRandomValue(ResX*-1,ResX)),
+                     static_cast<float>(GetRandomValue(ResY*-1,ResY))},
+            (Vector3){static_cast<float>(GetRandomValue(-200,200)),
+                     static_cast<float>(GetRandomValue(-200,200)),0},
+            name,
+        };
+        
+        pl.color = (Color){
+            static_cast<uint8_t>(GetRandomValue(0,255)),
+            static_cast<uint8_t>(GetRandomValue(0,255)),
+            static_cast<uint8_t>(GetRandomValue(0,255)),
+            255
+        };
+
+        Radar::Planes.push_back(pl);
+        
+        delete[] name;
+        PlaneSpawnTime = 1;
+    }
+    Radar::PlaneSpawnTime -= DeltaTime; 
+}
+
+void Radar::DestroyPlane(uint16_t VectorPosition) {
+    Radar::Planes.erase(Radar::Planes.begin() + VectorPosition);
+}
+
+void Radar::UpdatePlanes() {
+    for(uint16_t i = 0; i < Radar::Planes.size(); ++i) {
+        if(Planes.at(i).Position.x > ResX * 3  ||
+           Planes.at(i).Position.x < ResX * -3 ||
+           Planes.at(i).Position.y > ResY * 3  ||
+           Planes.at(i).Position.y < ResY * -3 ) DestroyPlane(i);
+
+        Radar::Planes.at(i).Position.x += Radar::Planes.at(i).Velocity.x * DeltaTime;
+        Radar::Planes.at(i).Position.y += Radar::Planes.at(i).Velocity.y * DeltaTime;
+    }
+}
+
+void Radar::UpdateEvent() {
+    if(IsKeyDown(KEY_UP)   && Camera.zoom < 1)   Camera.zoom += 1 * DeltaTime;
+    if(IsKeyDown(KEY_DOWN) && Camera.zoom > 0.1) Camera.zoom -= 1 * DeltaTime;
+}
--- src/radarcore.hpp
+++ src/radarcore.hpp
@@ -1,15 +1,34 @@
 #pragma once
+#include <cstdint>
 #include <raylib.h>
 #include <stdint.h>
+#include <vector>
+#include <string>
 
 class Radar {
     private:
+        typedef struct Plane {
+            Vector2 Position;
+            Vector3 Velocity;
+            std::string name;
+            Color color;
+        } Plane;
+        
+        uint16_t ResX;
+        uint16_t ResY;
         Camera2D Camera;
-
+        std::vector<Plane> Planes;
+        float PlaneSpawnTime;
+        float DeltaTime;
+        
+        void UpdateRender();
+        void UpdatePlanes();
+        void UpdateEvent();
+        void SpawnPlane();
+        void DestroyPlane(uint16_t VectorPosition);
     public:
         Radar(uint16_t ResX, uint16_t ResY);
         ~Radar();
 
         void Run();
-        void UpdateRender();
 }; 
